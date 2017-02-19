22f83fe2751d1650caf8bbc3c07c229e9727b2ff
lyes kherbiche <kerbiche@gmail.com>
2017-02-19 17:52:00 +0100

    web mod conf

---
+++ spboong-web/src/main/java/com/ima/proj/web/ctrl/HomeCtrl.java
@@ -0,0 +1,13 @@
+package com.ima.proj.web.ctrl;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author lyes ⴿⴻⵔⴱⵉⵛ
+ *
+ */
+@RestController
+public class HomeCtrl {
+
+}
